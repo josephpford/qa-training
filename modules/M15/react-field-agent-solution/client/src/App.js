5e9d57b4bf1ae77097dc1d5ab7ac992a10f582ce
Joe Ford <JoFord@dev-10.com>
2022-06-20 12:03:35 -0500

    Adding react-field-agent-solution and removing solar-farm zip

---
+++ modules/M15/react-field-agent-solution/client/src/App.js
@@ -0,0 +1,12 @@
+import Agents from './components/Agents';
+
+function App() {
+  return (
+    <>
+      <h1 className="py-2">Field Agents</h1>
+      <Agents />
+    </>
+  );
+}
+
+export default App;
